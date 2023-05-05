e94f8931d27d14490b01af42700c1f64c722146b
Kinvalan <sebastianmoller01@icloud.com>
2023-05-05 15:06:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*
+Lag en konsoll-app som skriver til konsollen "Hei, hva heter du?",
+
+deretter leser inn input fra bruker og til slutt printer ut "Velkommen + det brukeren skrev"
+
+eks.
+
+Hei, hva heter du?
+
+Joakim
+
+Velkommen Joakim!
+*/
+
+
+namespace ReadLine_and_WriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, hva heter du?");
+
+            var input = Console.ReadLine(); // ReadLine forventer en verdi fra bruker
+
+            Console.WriteLine("Velkommen " + input + "!"); // Verdien bruker skrev blir skrevet til konsollen
+        }
+    }
+}
